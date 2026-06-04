03d5527a815571194a897f024432a32b8e910909
Harshit-Sharma-0 <hs90908080@gmail.com>
2026-06-04 12:38:10 +0530

    fix: move supabase keys to env variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jmvyyurqddhluungbudy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptdnl5dXJxZGRobHV1bmdidWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODMwNTksImV4cCI6MjA5NTU1OTA1OX0.R-4QU1bdxHQtT8Gbl8YaizrEuQXF9A7LWvJVWUC9toU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
